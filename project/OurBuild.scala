919947e788abc46b8867ade2b0b92a19972409f0
Jessica Gonzalez <jessica@loyal3.com>
2013-10-30 14:05:39 -0700

    Switched to a Scala build file, allow for publishing to specified repository.

---
+++ project/OurBuild.scala
@@ -0,0 +1,29 @@
+import sbt.Keys._
+import sbt._
+
+object OurBuild  extends Build {
+  override lazy val settings = super.settings ++ Seq(
+    organization := "com.loyal3",
+    version := Option(System.getenv("BUILD_VCS_NUMBER")).getOrElse("DEV"),
+    scalaVersion := "2.9.2",
+    sbtPlugin := true,
+    publishTo := Option(System.getenv("PUBLISH_REPOSITORY_URL")).map("remote" at),
+    publishMavenStyle := false,
+    creds
+  )
+
+  lazy val root = Project(
+    id = "sbt-package-deb",
+    base = file("."),
+    settings = Project.defaultSettings
+  )
+
+  lazy val creds = credentials ++= (for {
+    realmName <- Option(System.getenv("CREDENTIALS_REALM_NAME"))
+    realmUrl  <- Option(System.getenv("CREDENTIALS_REALM_URL"))
+    username  <- Option(System.getenv("CREDENTIALS_USERNAME"))
+    password  <- Option(System.getenv("CREDENTIALS_PASSWORD"))
+  } yield {
+    Credentials(realmName, realmUrl, username, password)
+  }).toSeq
+}

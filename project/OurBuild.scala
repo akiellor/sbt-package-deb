e09a5b5dc9932fa40bcb63f43f720505ccb3c405
Jessica Gonzalez <jessica@loyal3.com>
2013-10-30 15:56:53 -0700

    Changed scala version to 2.9.2

--- project/OurBuild.scala
+++ project/OurBuild.scala
@@ -5,7 +5,7 @@ object OurBuild  extends Build {
   override lazy val settings = super.settings ++ Seq(
     organization := "com.loyal3",
     version := Option(System.getenv("BUILD_VCS_NUMBER")).getOrElse("DEV"),
-    scalaVersion := "2.9.3",
+    scalaVersion := "2.9.2",
     sbtPlugin := true,
     publishTo := Option(System.getenv("PUBLISH_REPOSITORY_URL")).map("remote" at),
     publishMavenStyle := false,
